6fdc62710acccb4efa337b3e379342090adc1183
CodeGeek <codyleng22@gmail.com>
2018-03-26 22:53:47 +1100

    Update mission.go

--- mission.go
+++ mission.go
@@ -17,15 +17,13 @@ func cal_square(x int) int{
 
 /*sum up the single case. Due to the requirements of the test, the recursive method is applied*/
 func single_case_sum(x int) int{
-    var num,sum int;
+    var num int;
     //fmt.Scanf("%d", &num)
     if (x > 0) {
         fmt.Scanf("%d", &num);
-        sum = cal_square(num) + single_case_sum(x-1);
-    } else {   
-        sum = 0;      
-    }
-    return sum;
+        return cal_square(num) + single_case_sum(x-1);
+    } 
+    return 0;
 }
 
 /*display the result of each case*/
